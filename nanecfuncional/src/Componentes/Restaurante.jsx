6924d18c481155fcee87e625e774c3c4b851d883
Pato677 <168039886+Pato677@users.noreply.github.com>
2025-05-27 22:05:57 -0500

    feat: Add like and dislike functionality to Restaurante component
    - Updated App.js to manage total likes state and pass like/dislike handlers to Restaurante component.
    - Modified Restaurante.jsx to handle individual likes and dislikes, displaying counts and buttons for interaction.
    - Adjusted package.json to update dependencies for React and testing libraries.

--- nanecfuncional/src/Componentes/Restaurante.jsx
+++ nanecfuncional/src/Componentes/Restaurante.jsx
@@ -1,21 +1,49 @@
 import React, { useState } from "react";
 
+
+
+
 const Restaurante = (props) => {
-    const { nombre, direccion, tipo, url } = props;
+    const { nombre, direccion, tipo, url, onlike, onDislike } = props;
+
+    /*
+    const reaccion = [
+        { tipo: "like", contador: 0 },
+        { tipo: "dislike", contador: 0 }
+    ]
+    */
     const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
 
     const handlerLike = () => {
-        setLikes(likes + 1);
-    };
+       
+        setLikes((prevstate) => {
+            return (prevstate + 1)
+        });
+        onlike()
 
+    }
+       
+       
+    const handlerDislike = () => {
+        setDislikes((prevstate => {
+            return (prevstate + 1)
+        }));
+        onDislike()
+    }
+  
     return (
         <div className="restaurante">
+            
             <h1>{nombre}</h1>
             <h2>{direccion}</h2>
             <h3>{tipo}</h3>
             <img src={url} alt={nombre} />
             <h4>Likes: {likes}</h4>
             <button onClick={handlerLike}>Like</button>
+            <h4>Dislikes: {dislikes}</h4>
+            <button onClick={handlerDislike}>Dislike</button>
+            
         </div>
     );
 };
