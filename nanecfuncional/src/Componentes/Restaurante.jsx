a8eac22f46d088229b7bf5fba75063923173ccfe
Pato677 <168039886+Pato677@users.noreply.github.com>
2025-05-28 08:30:19 -0500

    feat: Implement like and dislike handling in Restaurante component

--- nanecfuncional/src/Componentes/Restaurante.jsx
+++ nanecfuncional/src/Componentes/Restaurante.jsx
@@ -1,49 +1,63 @@
 import React, { useState } from "react";
 
-
-
-
 const Restaurante = (props) => {
     const { nombre, direccion, tipo, url, onlike, onDislike } = props;
+    ///usando un vector de estado para manejar los likes y dislikes
+    const [preferencia, setpreferencia] = useState({
+        likes: 0,
+        dislikes: 0
+    });
 
-    /*
-    const reaccion = [
-        { tipo: "like", contador: 0 },
-        { tipo: "dislike", contador: 0 }
-    ]
-    */
+    const handlerLike = () => {
+        setpreferencia((prevState) => ({
+            ...prevState, 
+            likes: prevState.likes + 1
+        }));
+        onlike();
+    };
+    const handlerDislike = () => {
+        setpreferencia((prevState) => ({
+            ...prevState,
+            dislikes: prevState.dislikes + 1
+        }));
+        onDislike();
+   
+    };
+//definiendo como se comporta el componente Restaurante
+
+/*
     const [likes, setLikes] = useState(0);
     const [dislikes, setDislikes] = useState(0);
 
     const handlerLike = () => {
-       
+
         setLikes((prevstate) => {
             return (prevstate + 1)
         });
         onlike()
 
     }
-       
-       
+
+
     const handlerDislike = () => {
         setDislikes((prevstate => {
             return (prevstate + 1)
         }));
         onDislike()
     }
-  
+*/
     return (
         <div className="restaurante">
-            
+
             <h1>{nombre}</h1>
             <h2>{direccion}</h2>
             <h3>{tipo}</h3>
             <img src={url} alt={nombre} />
-            <h4>Likes: {likes}</h4>
+            <h4>Likes: {preferencia.likes}</h4>
             <button onClick={handlerLike}>Like</button>
-            <h4>Dislikes: {dislikes}</h4>
+            <h4>Dislikes: {preferencia.dislikes}</h4>
             <button onClick={handlerDislike}>Dislike</button>
-            
+
         </div>
     );
 };
